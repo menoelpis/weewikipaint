ed6537b0dacd9f02a67447214c5bddd023a3e8c1
Daniel Park <menoelpis@gmail.com>
2016-03-20 10:45:10 +0900

    Build fails if lint fails

--- jakefile.js
+++ jakefile.js
@@ -34,7 +34,8 @@ task("lint", [], function () {
     files.include("**/*.js");
     files.exclude("node_modules");
     var options = nodeLintOptions();
-    lint.validateFileList(files.toArray(), options, {});
+    var passed = lint.validateFileList(files.toArray(), options, {});
+    if (!passed) fail("Lint failed");
 });
 
 desc("Integrate");
@@ -44,6 +45,7 @@ task("integrate", ["default"], function() {
     console.log("   a. Walk over to integration box.");
     console.log("   b. 'git pull'");
     console.log("   c. 'jake'");
+    console.log("   d. If jake fails. stop and try again!");
     console.log("3. 'git checkout integration'");
     console.log("4. 'git merge master --no-ff --log'");
     console.log("5. 'git checkout master'");
