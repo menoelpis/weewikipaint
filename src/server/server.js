0c9a95a722861cb6b18c262f1ad122550924bbb3
Daniel Park <menoelpis@gmail.com>
2016-03-20 21:24:57 +0900

    Removed instrumentation from server.js

--- src/server/server.js
+++ src/server/server.js
@@ -7,25 +7,13 @@ var http = require("http");
 var server;
 
 exports.start = function() {
-
-    console.log("start called");
     server = http.createServer();
-    console.log("server called");
-
     server.on("request", function(request, response) {
-       console.log("request called");
-
-        var body = "<html><head><title>Node HTTP</title></head>" +
-                "<body><p>This is a Node's HTTP Server</p></body></html>";
-        response.end(body);
+        response.end();
     });
-    console.log("'on' called");
-
     server.listen(8080);
-    console.log("listen called");
 };
 
 exports.stop = function(callback) {
-    console.log("stop called");
     server.close(callback);
 };
