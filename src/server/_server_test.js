ad0e3e71ff0e8c09c15779090769cd72b07bba94
Daniel Park <menoelpis@gmail.com>
2016-03-20 21:45:27 +0900

    Finished Basic Hello World Server - Still needs Cleanup

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -6,16 +6,27 @@
 var server = require("./server.js");
 var http = require("http");
 
-exports.tearDown = function(done) {
-    server.stop(function() {
+exports.tearDown = function (done) {
+    server.stop(function () {
         done();
     });
 };
 
-exports.testServerRespondsToGetRequest = function(test) {
+exports.test_serverReturnsHelloWorld = function (test) {
     server.start();
+    var request = http.get("http://localhost:8080");
+    request.on("response", function (response) {
+        var receivedData = false;
+        response.setEncoding("utf8");
 
-    http.get("http://localhost:8080", function(response) {
-        test.done();
+        test.equals(200, response.statusCode, "status code");
+        response.on("data", function (chunk) {
+            receivedData = true;
+            test.equals("Hello World", chunk, "response text");
+        });
+        response.on("end", function () {
+            test.ok(receivedData, "should have received response data");
+            test.done();
+        });
     });
 };
